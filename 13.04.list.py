2efacf8d19f402067e812ac7e0886ec5a258d549
bzhumakova <bzhumakova@gmail.com>
2021-04-19 17:33:02 +0600

    work with str and random commands

---
+++ 13.04.list.py
@@ -0,0 +1,139 @@
+# count=0
+# while count<100:
+#     count+=1
+#     print(count)
+
+
+
+# count=0
+# sum=0
+# while count<100:
+#     count+=1
+#     sum+=count
+#     print(count,sum)
+
+# str1='milk'
+# print(str1[2])
+
+# str1='milk'
+# i=0
+# while i<len(str1):
+#     print(str1[i])
+#     i=i+1
+
+
+# str1='milk'
+# i=0
+# while i<len(str1):
+#     print(str1[i:])
+#     i=i+1
+
+# i=0
+# sum=0
+# while i<10:
+#     i=i=1
+#     sum=sum+1
+#     if sum==45:
+#         break
+# print(sum)
+
+# import random
+# i=0
+# sum=0
+# secret=random.randint(1,3)
+# print(secret)
+# while i<secret:
+#     i+=1
+#     sum+=i
+# print(secret)
+# print(sum)
+
+# import random
+# i=0
+# secret=0
+# j=0
+# while i<10:
+#     secret=random.randint(1,1000)
+#     print('Случайное число ',secret,'под номером',i)
+#     if secret>j:
+#         j=secret
+#         print(j)
+#     i+=1
+#     print('Максимальное число:',j)
+
+# import random
+# i=0
+# secret=0
+# j=1000
+# while i<10:
+#     secret=random.randint(1,1000)
+#     print('Случайное число ',secret,'под номером',i)
+#     if secret<j:
+#         j=secret
+#         print(j)
+#     i+=1
+#     print('Минимальное число:',j)
+
+
+# years=5
+# money=int(input("Введиете сумму: "))
+# percentage=10
+#
+# i=0
+# if money>0:
+#     while i<years:
+#         i+=1
+#         money=money+(money*percentage/100)
+#     print(money)
+# else:
+#     print("Недопустимая сумма! ")
+
+# i=0
+# while i<1000000:
+#     i+=1
+#     print(i)
+
+# list1=["Vova",2004,'M',"18",['harry','das',"asda"]]
+# print(type(list1[1]))
+
+
+# list1=["Vova",2004,'M',"18",['harry','das',"asda"]]
+# print(list1[4][0])
+
+# list1=["Vova",2004,'M',"18",['harry','das',"asda"]]
+# i=0
+# while i<len(list1):
+#     print(list1[i])
+#     i+=1
+
+# list1=["Begimai","johny","Frank","Fred","Obama","Trump","Boris","Steven","Cat"]
+# criminal=input("Enter criminal's name: ")
+# if criminal in list1:
+#     print('Преступник найден!')
+# else:
+#     print("Город чист!")
+# i=0
+# while i<len(list1):
+#     print(list1[i])
+#     i+=1
+
+# list1=["Begimai","johny","Frank","Fred","Obama","Trump","Boris","Steven","Cat"]
+# criminal=input("Enter criminal's name: ")
+# i=0
+# while i<len(list1):
+#     if criminal in list1:
+#         if criminal==list1[i]:
+#             print('Адрес криминала: ',i)
+#             break
+#     else:
+#         print("Не найден!")
+#         break
+#     i+=1
+
+
+
+
+
+
+
+
