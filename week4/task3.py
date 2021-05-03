7a75082e3d8b9c0a889e08d7d72cdaee0e89a302
bzhumakova <bzhumakova@gmail.com>
2021-05-03 18:59:00 +0600

    requests of commits from GitHub

---
+++ week4/task3.py
@@ -0,0 +1,39 @@
+"""
+1. Вывести все коммиты (сообщения) и подсчитать все.
+    1.1 Вывести сообщения длина которых больше 10 и их кол-во
+
+"""
+
+
+import requests
+url = 'https://api.github.com/repos/zhumakova/FirstProject/commits?page1&per_page=100'
+response = requests.get(url,headers={'Authorization':'Token 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'}).json()
+
+commit=[]
+
+for r in response:
+    commit.append(r['commit']['message'])
+    sum=0
+# print(commit)
+    count = 0
+    for massage in commit:
+        sum+=1
+
+
+        if len(massage)>10:
+            count+=1
+    print(massage)
+print('total commits-',sum)
+print('total commits that are great than 10-',count)
+user=[]
+for r in response:
+    if r['commit']['author']['name'] not in user:
+        user.append(r['commit']['author']['name'])
+print(user)
+    
+
+
+
+
+
+
